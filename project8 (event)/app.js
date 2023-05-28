c47615c3ffc4290e77b26876a02442d07e3a8888
aakarshan-raj <aakarshan7raj@gmail.com>
2023-05-28 22:35:12 +0530

    changes

--- project8 (event)/app.js
+++ project8 (event)/app.js
@@ -6,4 +6,8 @@ customEvent.on("death_of_all",()=>{
 	console.log("everyone is dead");
 })
 
+customEvent.on("death_of_all",()=>{
+	console.log("everyone is alive");
+})
+
 customEvent.emit("death_of_all");
